aa0a6c255d3b4dfda34d23749215af605f57ef28
teptind <teptind@gmail.com>
2021-07-09 09:32:50 +0300

    LS-23 Обработка ошибок initial test fix, там не был определен Provider

--- src/tests/e2e/App.test.tsx
+++ src/tests/e2e/App.test.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import {render} from '@testing-library/react';
 import App from '../../App/App';
+import {store} from "../../redux/store/store";
+import UnhandledErrorBoundary from "../../components/boundary/UnhandledErrorBoundary";
+import {Provider} from "react-redux";
 
 test('app render', () => {
-    render(<App/>);
+    render(
+        <Provider store={store}>
+            <UnhandledErrorBoundary>
+                <App/>
+            </UnhandledErrorBoundary>
+        </Provider>
+    );
     // const linkElement = screen.getByText(/learn react/i);
     // expect(linkElement).toBeInTheDocument();
 });
