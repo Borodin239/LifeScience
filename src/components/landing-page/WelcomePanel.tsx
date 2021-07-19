f8a9fa51cb99deac18e61a789ba0f8d802081310
Denis Reznichenko <Denis.Reznichenko@jetbrains.com>
2021-07-19 11:59:01 +0300

    LS-78 Лицевая страница: макет // add text and styling for welcome panel

--- src/components/landing-page/WelcomePanel.tsx
+++ src/components/landing-page/WelcomePanel.tsx
@@ -1,13 +1,43 @@
 import React from "react";
-import {Box} from "@material-ui/core";
+import {Box, InputBase, Paper} from "@material-ui/core";
 import {useWelcomePanelStyles} from "./useWelcomePanelStyles";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import {useHistory} from "react-router-dom";
+import appRoutesNames from "../../infrastructure/common/appRoutesNames";
+import SearchIcon from "@material-ui/icons/Search";
 
 const WelcomePanel = () => {
     const classes = useWelcomePanelStyles()
+    const history = useHistory()
+    const handleBrowseClick = () => {
+        history.push(appRoutesNames.ROOT)
+    }
 
     return (
         <Box className={classes.mainContainer}>
-
+            <Box className={classes.centerContainer}>
+                <Typography variant={'h3'} className={classes.mainName}>
+                    JetScience
+                </Typography>
+                <Typography variant={'h5'}>
+                    is a simple and easy-to-use
+                    catalog of research protocols.
+                </Typography>
+                <Button onClick={handleBrowseClick} className={classes.browseButton}>
+                    Browse the catalog
+                </Button>
+                <Typography variant={'h5'} className={classes.orText}>
+                    or
+                </Typography>
+                <Paper component="form" className={classes.searchPaper}>
+                    <SearchIcon className={classes.iconButton}/>
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Search for the method"
+                    />
+                </Paper>
+            </Box>
         </Box>
     )
 }
