aef5d3d70eb3376326ce099b2080a9a19452bf79
aosyu <ann.syuris@gmail.com>
2022-01-12 19:12:49 +0300

    rename category page partly fixed

--- frontend/src/components/categories/admin/dialogs/RenameCategory/RenameCategoryDialog.tsx
+++ frontend/src/components/categories/admin/dialogs/RenameCategory/RenameCategoryDialog.tsx
@@ -1,24 +1,26 @@
-import React, {useState} from "react";
+import React, {useCallback, useEffect, useState} from "react";
 import {CategoryDialogProps} from "../CreateCategory/CreateCategoryDialog";
 import {Dialog, DialogTitle, TextField} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {useStyles} from "../dialog-styles";
-import {useAppDispatch, useAppSelector} from "../../../../../redux/hooks";
+import {useAppDispatch} from "../../../../../redux/hooks";
 import {useHistory} from "react-router-dom";
-import {updateCategory} from "../../../../../redux/categories/thunkActions";
+import {getCategoryInfoThunk, updateCategory} from "../../../../../redux/categories/thunkActions";
 import splitThunkPayload from "../../../../../redux/utils/splitThunkPayload";
 import handleThunkErrorBase from "../../../../../redux/utils/handleThunkErrorBase";
 import Alert from "@material-ui/lab/Alert";
-import apiConstants from "../../../../../infrastructure/http/api/apiConstants";
+import {pathMove, ROOT_NAVIGATION_UNIT} from "../../../../../redux/navigation/slice";
+import {getRedirectionRoute} from "../../../../../infrastructure/ui/utils/BreadcrumbsNavigationUtils";
+import {CategoryInfoView} from "../../../../../infrastructure/http/api/view/category/CategoryInfoView";
 
-const RenameCategoryDialog: React.FC<CategoryDialogProps> = (props) => {
+const RenameCategoryDialog: React.FC<CategoryDialogProps> = ({categoryId, categoryName, onClose, isOpen}) => {
 
     const classes = useStyles()
     const dispatch = useAppDispatch()
     const [alertText, setAlertText] = useState<string | null>(null)
     const history = useHistory()
     const [newName, setNewName] = useState<string>('')
-    const parentId = useAppSelector(state => state.navigationReducer.path).map(i => i.route.split("/").pop()).reverse()[1]
+    // const [currentCategoryName, setCategoryName] = useState<string>(categoryName!)
 
     const handleRenameClick = () => {
         dispatch(updateCategory({
@@ -27,12 +29,17 @@ const RenameCategoryDialog: React.FC<CategoryDialogProps> = (props) => {
                 aliases: [],
                 parentsToAdd: [],
                 parentsToDelete: []
-            }, id: props.categoryId.toString()
+            }, id: categoryId.toString()
         }))
             .unwrap()
             .then(payload => splitThunkPayload(payload))
-            .then(() => props.onClose())
-            .then(() => history.replace(`${apiConstants.routes.categories.INITIAL}/${parentId}`)) // TODO
+            .then(() => onClose())
+            .then(() => dispatch(pathMove({
+                name: newName,
+                route: getRedirectionRoute({type: 'category', categoryId: categoryId.toString()}),
+                type: 'category'
+            })))
+            .then(() => categoryName=newName)
             .catch(thunkError => {
                 if (thunkError.name === 'ApiError' && thunkError.description.httpCode === 400) {
                     setAlertText(thunkError.description.message);
@@ -42,10 +49,31 @@ const RenameCategoryDialog: React.FC<CategoryDialogProps> = (props) => {
             })
     }
 
+    // const tmp = useCallback(() =>
+    //       dispatch(getCategoryInfoThunk(categoryId))
+    //         .unwrap()
+    //         .then(payload => splitThunkPayload(payload))
+    //         .then(() => alert(newName))
+    //         .catch(thunkError => {
+    //             // setIsCategoryLoading(false);
+    //             if (thunkError.name === 'ApiError' && thunkError.description.httpCode === 404) {
+    //                 history.push(ROOT_NAVIGATION_UNIT.route);
+    //             } else {
+    //                 handleThunkErrorBase(thunkError, history, dispatch);
+    //             }
+    //         }), [dispatch])
+    // // useEffect(() =>
+    // // tmp(), [tmp])
+
     return (
-        <Dialog open={props.isOpen} onClose={() => { props.onClose(); setAlertText(null) }} classes={{paper: classes.paper}}>
-            <DialogTitle>Rename "{props.categoryName}" category</DialogTitle>
-            <TextField variant="outlined" placeholder="Enter new name" onChange={(e) => setNewName(e.target.value)}/>
+        <Dialog open={isOpen} onClose={() => {
+            onClose();
+            setAlertText(null)
+        }} classes={{paper: classes.paper}}>
+            <DialogTitle>Rename "{categoryName}" category</DialogTitle>
+            <TextField variant="outlined" placeholder="Enter new name" onChange={(e) =>
+                setNewName(e.target.value)
+            }/>
             <Button className={classes.submit} onClick={handleRenameClick}>Rename</Button>
             {alertText &&
             <Alert severity="error" style={{marginTop: 10}}>
