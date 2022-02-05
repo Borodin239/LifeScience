3319fad7ee95b1ab72dbbed1e8986bda712e3529
aosyu <ann.syuris@gmail.com>
2022-02-05 18:20:12 +0300

    DeleteCategoryDialog: DeleteDialog component added

--- frontend/src/components/categories/admin/dialogs/DeleteCategory/DeleteCategoryDialog.tsx
+++ frontend/src/components/categories/admin/dialogs/DeleteCategory/DeleteCategoryDialog.tsx
@@ -1,17 +1,14 @@
 import React, {useState} from "react";
 import {CategoryDialogProps} from "../CreateCategory/CreateCategoryDialog";
-import {Box, Dialog, DialogTitle} from "@material-ui/core";
-import Button from "@material-ui/core/Button";
-import {useStyles} from "../dialog-styles";
 import {useAppDispatch, useAppSelector} from "../../../../../redux/hooks";
 import {deleteCategory} from "../../../../../redux/categories/thunkActions";
 import splitThunkPayload from "../../../../../redux/utils/splitThunkPayload";
 import {useHistory} from "react-router-dom";
 import apiConstants from "../../../../../infrastructure/http/api/apiConstants";
-import Alert from "@material-ui/lab/Alert";
 import {pathMove} from "../../../../../redux/navigation/slice";
 import {getRedirectionRoute} from "../../../../../infrastructure/ui/utils/BreadcrumbsNavigationUtils";
 import handleErrors from "../../../handleErrors";
+import DeleteDialog from "../../../../admin/dialogs/DeleteDialog";
 
 const DeleteCategoryDialog: React.FC<CategoryDialogProps> = ({
                                                                  categoryId,
@@ -19,8 +16,6 @@ const DeleteCategoryDialog: React.FC<CategoryDialogProps> = ({
                                                                  isOpen,
                                                                  categoryName
                                                              }) => {
-
-    const classes = useStyles()
     const dispatch = useAppDispatch()
     const history = useHistory()
     const [alertText, setAlertText] = useState<string | null>(null)
@@ -47,29 +42,9 @@ const DeleteCategoryDialog: React.FC<CategoryDialogProps> = ({
     }
 
     return (
-        <Dialog open={isOpen} onClose={() => {
-            onClose();
-            setAlertText(null)
-        }} classes={{paper: classes.paper}}>
-            <DialogTitle>
-                Are you sure you want to delete "{categoryName}" category?
-            </DialogTitle>
-            <Box className={classes.twoButtonsPanel}>
-                <Button className={classes.yesButton} onClick={handleDeleteButton}>
-                    Yes
-                </Button>
-                <Button onClick={() => {
-                    onClose();
-                    setAlertText(null)
-                }} className={classes.noButton}>
-                    No, go back
-                </Button>
-            </Box>
-            {alertText &&
-                <Alert severity="error" style={{marginTop: 10}}>
-                    {alertText}
-                </Alert>}
-        </Dialog>
+        <DeleteDialog id={categoryId} onClose={onClose} isOpen={isOpen} name={categoryName}
+                      handleDeleteButton={handleDeleteButton}
+                      type={'category'} alertText={alertText} setAlertText={setAlertText}/>
     )
 }
 export default DeleteCategoryDialog
