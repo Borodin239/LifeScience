410c4fbe03b6ef8a7d0f7ddae85e1ce8c3be6bb8
AnastasiiaGorelova <anastasia.gorelova2003@gmail.com>
2022-02-15 21:30:31 +0300

    delete extra imports

--- backend/src/test/kotlin/com/jetbrains/life_science/container/approach/published/service/PublicApproachServiceTest.kt
+++ backend/src/test/kotlin/com/jetbrains/life_science/container/approach/published/service/PublicApproachServiceTest.kt
@@ -10,14 +10,8 @@ import com.jetbrains.life_science.container.approach.utilities.assertContainsSec
 import com.jetbrains.life_science.container.approach.utilities.assertNotContainsSection
 import com.jetbrains.life_science.exception.not_found.ApproachNotFoundException
 import com.jetbrains.life_science.exception.search_unit.ApproachSearchUnitNotFoundException
-import com.jetbrains.life_science.search.query.SearchUnitType
-import com.jetbrains.life_science.search.result.approach.ApproachSearchResult
-import com.jetbrains.life_science.search.service.SearchService
-import com.jetbrains.life_science.search.service.maker.makeSearchQueryInfo
 import com.jetbrains.life_science.section.service.SectionService
 import com.jetbrains.life_science.user.credentials.service.CredentialsService
-import com.jetbrains.life_science.util.populator.ElasticPopulator
-import org.elasticsearch.client.RestHighLevelClient
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
@@ -26,7 +20,6 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 import org.springframework.transaction.annotation.Transactional
-import javax.annotation.PostConstruct
 
 @SpringBootTest
 @Sql(
