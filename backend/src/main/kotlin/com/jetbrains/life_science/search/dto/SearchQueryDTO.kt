ed43fc9514bff6371674c24e595fe878ba2be6fa
AnastasiiaGorelova <anastasia.gorelova2003@gmail.com>
2022-02-26 20:23:04 +0300

    correct validation of queries by id

--- backend/src/main/kotlin/com/jetbrains/life_science/search/dto/SearchQueryDTO.kt
+++ backend/src/main/kotlin/com/jetbrains/life_science/search/dto/SearchQueryDTO.kt
@@ -2,15 +2,14 @@ package com.jetbrains.life_science.search.dto
 
 import org.hibernate.validator.constraints.Length
 import javax.validation.constraints.Max
+import javax.validation.constraints.Pattern
 import javax.validation.constraints.Positive
 import javax.validation.constraints.PositiveOrZero
 
 data class SearchQueryDTO(
 
-    @field:Length.List(
-        Length(min = 2, message = "minimum message length is 2 characters"),
-        Length(max = 77, message = "maximum message length is 77 characters")
-    )
+    @field:Length(max = 77, message = "maximum message length is 77 characters")
+    @field:Pattern(regexp = "[1-9]|..+", message = "minimum message length is 2 characters")
     val text: String,
 
     val includeTypes: List<String> = listOf("CATEGORY", "APPROACH", "PROTOCOL"),
